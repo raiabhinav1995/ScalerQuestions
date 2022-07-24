892270ae15f4adf11a1ec5ba9a6c680bf6e1d523
Abhinav <raiabhinav1995@gmail.com>
2022-07-24 21:05:40 +0530

    Added condition

--- InvertTree.java
+++ InvertTree.java
@@ -1,5 +1,6 @@
 public class InvertTree {
     public static void invertTree(Tree root) {
+        if (root == null) return;
         if (root.left == null && root.right != null || root.left != null && root.right == null || root.left != null && root.right != null) {
             Tree temp = root.left;
             root.left = root.right;
@@ -13,14 +14,12 @@ public class InvertTree {
 
     public static void main(String[] args) {
 
-        Tree root = new Tree(1);
-        root.left = new Tree(2);
-
-        root.left.left = new Tree(4);
-        root.left.right = new Tree(5);
-        root.right = new Tree(3);
-        root.right.left = new Tree(6);
-        root.right.right = new Tree(7);
+        Tree root = new Tree(5);
+        root.left = new Tree(1);
+        root.left.left = new Tree(2);
+        //root.right = new Tree(3);
+        //root.right.left = new Tree(6);
+//        root.right.right = new Tree(7);
         InorderTraversal.inorderTraversal(root);
         invertTree(root);
         InorderTraversal.inorderTraversal(root);
